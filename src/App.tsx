9751ce7b718b89997add6840c8b14fed70057b24
jacubas <58412645+Splitzer26@users.noreply.github.com>
2023-02-17 09:45:42 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Product } from './models/products.model'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let thisProduct : Product = {
+    id: 12,
+    name: "camara",
+    price: 145.54
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <div> {thisProduct.name} vale {thisProduct.price} lempiras y tiene el id: {thisProduct.id}
+
+        </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
